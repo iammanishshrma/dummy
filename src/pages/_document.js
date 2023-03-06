9c86ef84d4f1f622e6a6e14475f28446908ed27c
Manish Sharma <manish.sharma@sparxitsolutions.com>
2023-03-06 15:47:12 +0530

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,24 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head />
+            <Script
+                strategy="beforeInteractive"
+                src="https://accounts.google.com/gsi/client"
+                async
+            />
+            <Script
+                async
+                crossOrigin="anonymous"
+                src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v16.0&appId=758578365717574&autoLogAppEvents=1"
+                nonce="5zXzbKct"
+                strategy="beforeInteractive"
+            />
+            <Script
+                async
+                crossOrigin="anonymous"
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="beforeInteractive"
+            />
             <body>
                 <Main />
                 <div id="fb-root"></div>
